589cbb3ef9db83682c017ac58495be6916605185
nitinkohaal <nitink9915@gmail.com>
2024-09-22 22:42:49 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+function xyz() {
+   let a=document.getElementById("bdy").setAttribute("class","click1");
+}
+
+function xyz2(){
+   let b=document.getElementById("bdy").setAttribute("class","click2")
+}
+
